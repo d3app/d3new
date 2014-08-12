a9daf9dfc8e9e750cecd76162ff32ff0c0f92c40
d3app <namkyu@d3jubilee.com>
2014-08-12 00:33:41 -0700

    updates public

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module D3
   class Application < Rails::Application
+
+    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -21,3 +23,5 @@ module D3
     # config.i18n.default_locale = :de
   end
 end
+
+
